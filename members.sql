641f77c56842d0ae4a8908a320de6db683f179e6
EmpthyLL <javileh40@gmail.com>
2024-08-14 08:51:53 +0700

    first commit

--- members.sql
+++ members.sql
@@ -1,1 +1,13 @@
 create database members;
+
+CREATE TABLE `members`.`members` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(100) NOT NULL,
+  `username` VARCHAR(50) NOT NULL,
+  `role` VARCHAR(45) NULL,
+  `roleid` VARCHAR(10) NULL,
+  `bgcolor` VARCHAR(45) NULL,
+  `status` VARCHAR(100) NULL,
+  `statusid` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`)
+);
