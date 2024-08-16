f1cf57713d3f877e13ee74024175c908809aae1e
EmpthyLL <javileh40@gmail.com>
2024-08-16 08:41:41 +0700

    Config

--- routes/team-controller.js
+++ routes/team-controller.js
@@ -1,8 +1,30 @@
 function teamManager(app, opts, done){
-    app.get('/',{},(req,res) => {
-        res.send({
-            message:'HI!'
-        })
+    app.get('/', {}, async (req, res) => {
+        const key = req.params.id
+        try {
+            const [members] = await app.mysql.execute("SELECT * FROM members")
+            res.send(members)
+        } catch (error) {
+            if(error){
+                res.status(400).send({
+                    message:error.message
+                })
+            }
+        }
+    })    
+    app.post('/', {}, async (req, res) => {
+        const {username,email,role,roleid,bgColor,status,statusid} = req.body
+        try {
+            await app.mysql.execute('INSERT INTO books VALUES (?, ?, ?, ?, ?, ?, ?)', [username,email,role,roleid,bgColor,status,statusid]);
+            const [members] = await app.mysql.execute("SELECT * FROM members");
+            res.send(members);
+        } catch (error) {
+            if(error){
+                res.status(400).send({
+                    message:error.message
+                })
+            }
+        }
     })
     done()
 }
--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const appConfig = require('./config/app')
 const DBConfig = require('./config/database')
 const logger = require('./logger')
 const fastify = require('fastify')
+const mysql = require('@fastify/mysql')
 const fastifycors = require('@fastify/cors')
 const { teamManager } = require('./routes/team-controller')
 const app = fastify({logger:logger[appConfig.appMode]})
 const port = appConfig.appPort
+
+app.register(mysql,{
+  host:DBConfig.connection.host,
+  port:DBConfig.port,
+  user:DBConfig.connection.user,
+  password:DBConfig.connection.password,
+  database:'bookstore',
+  promise: true
+})
 
 app.register(fastifycors)
 app.register(teamManager,{prefix:'/team'})
